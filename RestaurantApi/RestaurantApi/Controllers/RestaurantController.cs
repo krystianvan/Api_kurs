da4621831d907fa4958ee036b3013eac97737492
krystianvan <65135056+krystianvan@users.noreply.github.com>
2021-06-09 20:37:49 +0200

    Add files via upload
    commit 1

---
+++ RestaurantApi/RestaurantApi/Controllers/RestaurantController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApi.Entities;
+using RestaurantApi.Models;
+using RestaurantApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace RestaurantApi.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly IRestaurantService _restaurantService;
+
+        public RestaurantController(IRestaurantService restaurantService)
+        {
+            _restaurantService = restaurantService;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
+        {
+            var restaurantsDtos = _restaurantService.GetAll();
+
+            return Ok(restaurantsDtos);
+        }
+
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var id = _restaurantService.Create(dto);
+
+            return Created($"api/restaurant/{id}", null);
+        }
+
+
+
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<RestaurantDto>> Get([FromRoute] int id)
+        {
+            var restaurant = _restaurantService.GetById(id);
+
+            if (restaurant is null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<RestaurantDto>> Delete([FromRoute] int id)
+        {
+            var isDeleted = _restaurantService.Delete(id);
+            if(isDeleted)
+            {
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put([FromRoute] int id, [FromBody] EditRestaurantDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var isUpdated = _restaurantService.Put(id, dto);
+            if (isUpdated)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ RestaurantApi/RestaurantApi/Entities/RestaurantDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantApi.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        private string _connactionString = "Server=localhost\\SQLEXPRESS;Database=RestaurantDb;Trusted_Connection=True;";
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        //Nadanie że coś jest wymagane
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+
+        //Jaki typ bazy danych i jak połączyć
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connactionString);
+        }
+
+    }
+}
